6551f6e731eea02667c40c3a52c3582ee6d3f16d
Apirak Panatkool <apirakb@gmail.com>
2013-01-12 00:19:56 +0700

    create new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kuikui3::Application.config.secret_token = '10c032fa3a42b836c7f1acc6ca7fa08f7e8b2dce483e6badea148f0d1a71d731f39ec4c2d36ec5bcefc54a5968e3302eb46ecf5695fa3fc1cbe91a2a68dbeab4'
