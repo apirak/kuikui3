73fb967e3512210a0ff5b5c8a14a1e30306b331f
Apirak Panatkool <apirakb@gmail.com>
2013-01-16 09:41:19 +0700

    rspac on model

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Message do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "valid with complete content and tag" do
+  	@m = Message.new :content => "hello", :tags => "test, first"
+  	@m.should have(:no).errors
+    # Message.new(:content => "liquid nitrogen").should have(0).errors_on(:content)
+  end
+
+  it "fails validation with no content" do
+    @m = Message.new :tags => "test, first"
+    @m.should have(1).errors_on(:content)
+    @m.errors.on(:content).should == "is required"
+  end 
 end
